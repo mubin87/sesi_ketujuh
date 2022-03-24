0d203f51424df7ef05d386ce7f7da7de87797413
mubin87 <frans.andra.boys87@gmail.com>
2022-03-24 11:58:28 +0700

    sesi ke 7

---
+++ go.mod
@@ -0,0 +1,10 @@
+module projek_ketujuh
+
+go 1.18
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	gorm.io/gorm v1.23.3 // indirect
+)
---
+++ learning-gorm/models/product.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type Product struct {
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"not null;unique;type:varchar(191)"`
+	Brand     string `gorm:"not null;unique;type:varchar(191)"`
+	UserID    uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) (err error)  {
+	fmt.Println("Product Before Create()")
+	if len(p.Name) < 4 {
+		err = errors.New("product name is too short")
+	}
+
+	return
+}
